ecd867fec02ccba7638f40963ff5aac834fa2a40
Dmitriy <ambrose163@mail.ru>
2022-09-29 16:41:03 +0400

    save2

---
+++ zadacha66/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumAllNumbs(int min, int max)
+{
+ int sum = 0;
+    if (min == max)
+    {
+        return min;
+    }
+    sum += max;
+    return SumAllNumbs(min, max - 1) + sum;
+}
+Console.WriteLine(SumAllNumbs(1, 10));
