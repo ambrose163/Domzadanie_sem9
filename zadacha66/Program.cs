d35394bd5342b3e2c6cf041888e06d4ad985c6b1
Dmitriy <ambrose163@mail.ru>
2022-10-04 14:14:57 +0400

    save2

--- zadacha66/Program.cs
+++ zadacha66/Program.cs
@@ -1,13 +1,53 @@
-﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+//  в промежутке от M до N.
 
-int SumAllNumbs(int min, int max)
+using System;
+Console.Clear();
+
+int M = EnterInt("Enter integer number M: ");
+int N = EnterInt("Enter integer number N: ");
+
+int numMin = Min(M, N);
+int numMax = Max(M, N);
+string strNums = GetNaturalRowBetweenMN(numMin, numMax);
+string[] elemNums = strNums.Split(' ');
+
+int[] nums = Array.ConvertAll(elemNums, int.Parse);
+Console.WriteLine($"Сумма натур чисел в промежутке от {M} до {N} = {GetNumsSum(nums)}");
+
+
+int EnterInt(string prompt)
 {
- int sum = 0;
-    if (min == max)
-    {
-        return min;
-    }
-    sum += max;
-    return SumAllNumbs(min, max - 1) + sum;
+      Console.Write(prompt);
+      return int.Parse(Console.ReadLine()!);
 }
-Console.WriteLine(SumAllNumbs(1, 10));
+
+
+int Min(int a, int b)
+{
+      return a < b ? a : b;
+}
+
+int Max(int a, int b)
+{
+      return a > b ? a : b;
+}
+
+
+string GetNaturalRowBetweenMN (int numMin, int numMax)
+{
+      if (numMax == numMin)
+            return numMin.ToString();
+      
+      return numMin + " " + GetNaturalRowBetweenMN(numMin + 1, numMax); 
+}
+
+int GetNumsSum(int[] nums)
+{
+      int sum = 0;
+      for (int i = 0; i < nums.Length; i++)
+      {
+            sum += nums[i];
+      }
+      return sum;
+}
